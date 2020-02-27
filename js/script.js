4a28fffa7cb67c8808fcb7efc4f3e98e17cbc479
aqxin <aqxin.cs@gmail.com>
2020-02-26 19:47:54 -0800

    added hero to projects.html

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// navigation
+
+$("#toggle").click(function() {
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
+});
+
+$("#resize ul li a").click(function() {
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
+});
+
+$(".close-btn").click(function() {
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
+});
+
+// nav animation
+
+TweenMax.from("#brand", 1, {
+    delay: 0.4,
+    y: 10,
+    opacity: 0,
+    ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom("#menu li a", 1, {
+    delay: 0.4,
+    opacity: 0,
+    ease: Expo.easeInOut
+}, 0.1);
+
+new WOW().init();
